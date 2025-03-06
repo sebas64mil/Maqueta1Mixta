d9e631a25f6fa9761c828e522b1cf97f5ed08811
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-03-06 11:28:19 -0500

    avance 06/03/2025
    reaprendí como usar los inputs, y logre hacer conversión de masa

--- src/JS/Calculadora.js
+++ src/JS/Calculadora.js
@@ -53,6 +53,46 @@ cube.position.y=-2.2
 
 scene.background=new THREE.Color().setHex( 0x112233 )
 
+/*
+
+section de calculo de la calculadora
+*/
+
+let masaAuto1 = 0;  
+let tipoDeMasa1 = "kg"; 
+
+document.getElementById("MasaAuto_Aceleracion").addEventListener("input", (event) => {
+    masaAuto1 = parseFloat(event.target.value) || 0; 
+});
+document.getElementById("Masa_unidadesAuto1").addEventListener("input", function() {
+    tipoDeMasa1 = this.value || "kg"; 
+});
+
+function convertirAKilogramos(valor, unidad) {
+    let factorConversion = {
+        "mg": 0.000001,
+        "cg": 0.00001,
+        "g": 0.001,
+        "dg": 0.0001,
+        "hg": 0.1,
+        "kg": 1
+    };
+
+    let resultado = valor * (factorConversion[unidad] || 1);
+    console.log(`${valor} ${unidad} en kg es: ${resultado} kg`);
+}
+
+// Llamar la función cuando se haga clic
+document.getElementById("mostrarUnidades1").addEventListener('click', () => {
+    convertirAKilogramos(masaAuto1, tipoDeMasa1);
+});
+
+
+
+
+
+
+
 function animate() {
     
 
